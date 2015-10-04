c4f92c445db8e038cdd92bec226a5316665d54b0
Nicholas <nicholaschong12@gmail.com>
2015-10-04 17:07:35 +0800

    able to manage todo

--- app.js
+++ app.js
@@ -19,7 +19,65 @@ if(Meteor.isClient){
 				todoName.val("");
 			}
 		}
-	})
+	});
+	
+	Template.todoItem.events({
+		"click .delete-todo":function(e){
+			e.preventDefault();
+			if(confirm("Delete ? ")){
+			var documentId = this._id;
+			
+			Todos.remove({_id: documentId});
+			}
+		},
+		
+		"keyup [name=todoItem]":function(e){
+			if(event.which == 13 || event.which == 27)
+			{$(event.target).blur();}
+			else{
+			var documentId = this._id;
+			var todoItem = $(event.target).val();
+			Todos.update({_id: documentId}, {$set:{name: todoItem}});
+			//console.log("updated to "+todoItem);
+			}
+		},
+		
+		"change [type=checkbox]":function(e){
+			var documentId = this._id;
+			var isCompleted = this.completed;
+			if(isCompleted)
+			{
+				Todos.update({_id:documentId},{
+					$set:{completed:false}
+				});
+				console.log("Task marked as incomplete.");
+			}
+			else
+			{
+				Todos.update({ _id: documentId }, {$set: { completed: true }});
+				console.log("Task marked as complete.");
+			}
+		}
+	});
+	Template.todoItem.helpers({
+		'checked':function(){
+			var isCompleted = this.completed;
+			if(isCompleted){
+				return "checked";
+			}
+			else{
+				return "";
+			}
+		}
+	});
+	Template.todosCount.helpers({
+		totalTodos:function(){
+			return Todos.find().count();
+		},
+		completedTodos:function(){
+			return Todos.find({completed: true}).count();
+		}
+	});
 }
 if(Meteor.isServer){
 	
